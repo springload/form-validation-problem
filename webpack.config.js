5a200cf8668dfcf822f595abe78c805d2727f0c5
l-suzuki <lehsuzuki@gmail.com>
2018-06-05 17:52:09 +1200

    migrated css

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   entry: './index.js',
   output: {
-    path: path.join(__dirname, 'server/public'),
+    path: path.join(__dirname, './public'),
     filename: 'bundle.js'
   },
   module: {
